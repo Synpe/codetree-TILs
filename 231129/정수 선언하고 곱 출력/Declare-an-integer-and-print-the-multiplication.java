0c2d46c2173e590d83d20bcb67103cc459f567d2
Synpe <99089051+Synpe@users.noreply.github.com>
2023-11-29 12:59:58 +0900

    Title: 정수 선언하고 곱 출력, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231129/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a; 
+        int b;
+        a = 26;
+        b = 5;
+        System.out.print(a + " * " + b + " = " + 130);
+    }
+}
