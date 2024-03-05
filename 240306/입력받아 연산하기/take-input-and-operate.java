b4cb7a8137c0e7330c194c08e3bda8893eb0413f
Synpe <99089051+Synpe@users.noreply.github.com>
2024-03-06 08:35:16 +0900

    Title: 입력받아 연산하기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240306/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 13, b = 87;
+        a += 87;
+
+        System.out.println(a);
+
+        b %= 10;
+
+        System.out.print(b);
+    }
+}
