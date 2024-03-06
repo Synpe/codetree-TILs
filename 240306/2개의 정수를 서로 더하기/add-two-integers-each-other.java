5ef9846bfeebfe1061f5c58abd8fea9b520f7759
Synpe <99089051+Synpe@users.noreply.github.com>
2024-03-06 09:57:23 +0900

    Title: 2개의 정수를 서로 더하기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240306/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 3;
+
+        a = b+a;
+
+        System.out.print(a+" ");
+
+
+        b = a+b;
+
+        System.out.print(b);
+    }
+}
